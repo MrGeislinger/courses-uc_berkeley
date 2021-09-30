e9327e61783e4649b86a5f4bced54f844c4fa790
Victor Geislinger <9027783+MrGeislinger@users.noreply.github.com>
2021-09-29 21:27:33 -0700

    Load data from directory into DataFrame as a function
    - Use Python type hints
    - TODO: Update other files to match

--- load.py
+++ load.py
@@ -2,16 +2,23 @@ import pandas as pd
 import glob
 import os
 
-# Find path to data (CSVs)
-data_path = 'courses_csv'
-all_files = glob.glob(os.path.join(data_path, '*.csv'))
+def df_from_dir(data_path: str, printout: bool = False) -> pd.DataFrame:
+    '''
+    '''
+    all_files = glob.glob(os.path.join(data_path, '*.csv'))
 
-# Read all CSVs into one
-df_all = pd.concat(
+    # Read all CSVs into one
+    df_all = pd.concat(
         map(pd.read_csv, all_files), 
-        ignore_index=True
-)
+        ignore_index=True,
+    )
+    if printout:
+        print(f'Columns: {", ".join(df_all.columns)}')
+        print('Random Sample: ')
+        print(df_all.sample(5))
+    
+    return df_all
 
-print(f'Columns: {", ".join(df_all.columns)}')
-print('Random Sample: ')
-print(df_all.sample(5))
+if __name__ == '__main__':
+    # Find path to data (CSVs)
+    df = df_from_dir('courses_csv', printout=True)
